b9e84bc73d01c46a19d592981ab98f03278d9728
ostapkharysh <harysh@ucu.edu.ua>
2018-11-28 12:52:25 +0200

    adding explanation

--- poloniex_api.py
+++ poloniex_api.py
@@ -1,4 +1,7 @@
 from poloniex import Poloniex
+#https://pypi.org/project/poloniex/#
+
+
 #polo = Poloniex()
 #help(polo)
 
@@ -10,6 +13,7 @@ ticker = polo.returnTicker()['BTC_ETH']
 from poloniex import Poloniex
 import os
 
+# Шота нєрабочєє. Хз. Можливо ми зможемо без  цього обійтись. Бо тут пазоду для реальних ставок тільки таку апішку ннада.
 api_key = os.environ.get("CAXFKPES-ZVEVH9S2-68O93JFD-LHA72YW5")
 api_secret = os.environ.get("612fb1d36b098a4dfee559712115f9c0220c769b8a2a98b140d9f760885ee72d8e3d595a756c3b0b60ceeba60d59d18cc099a9dc6c4e5da51284104db9a5592e")
 polo = Poloniex(api_key, api_secret)
